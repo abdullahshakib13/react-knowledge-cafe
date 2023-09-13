0595224e0cf47dd534a40f8ce12b3314053e96e5
Abdullah Shakib <abdullahshakib13@gmail.com>
2023-09-13 19:50:21 +0600

    bookmark state and display bookmark

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
+import PropTypes from 'prop-types';
 
-const Blogs = () => {
+const Blogs = ({handleAddBookmark}) => {
     const [blogs, setBlogs] = useState([]);
     useEffect(() => {
         fetch('blogs.json')
@@ -10,9 +12,16 @@ const Blogs = () => {
     },[])
     return (
         <div className="md:w-2/3">
-            <h2 className="text-4xl">Blogs:{ blogs.length}</h2>
+            <h2 className="text-4xl">Blogs:{blogs.length}</h2>
+            {
+                blogs.map(blog=> <Blog key={blog.id} blog={blog} handleAddBookmark={handleAddBookmark}></Blog>)
+            }
         </div>
     );
 };
+
+Blogs.propTypes = {
+    handleAddBookmark: PropTypes.func
+}
 
 export default Blogs;
